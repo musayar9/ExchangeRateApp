7f79fcf01e4f02aa9415ef332b9555e19c814be4
Musa Sayar <96313325+musayar9@users.noreply.github.com>
2023-12-26 14:24:15 +0300

    Update script.js

--- script.js
+++ script.js
@@ -16,54 +16,34 @@ async function showMoney(){
     document.querySelector("#load").style.display = "none"
     try{
         const response = await fetch(url + "/codes");
-        console.log(response)
+       
         const data = await response.json();
-        console.log(data)
-
         const items = await data.supported_codes;
-        console.log(items)
-
         let options;
-
         for(let exchange of items){
             options += `
             <option value = ${exchange[0]}> ${exchange[1]}</option>`
-
 
             oneList.innerHTML = options;
             twoList.innerHTML = options
         }
 
     }catch(err){
-
+        throw new Error(err)
     }
-
-   
 }
 showMoney()
-
-
 exchangeCalculate.addEventListener("click", function(){
-
     document.querySelector("#load").style.display = "block"
     const exchange1 = oneValue.value;
     const exchange2 = twoValue.value;
     const  amount = moneyAmount.value;
-
-
     async function showExchange(){
         const resp = await fetch(url + "/latest/" + exchange1)
-        console.log(resp)
         const data = await resp.json();
-        console.log(data);
-
-
         const outpot = data.conversion_rates;
-
-        console.log(outpot)
-
         const result =  (data.conversion_rates[exchange2]*amount).toFixed(2);
-        console.log(result)
+    
      let valueResult;
         valueResult = `
         <div class="card border-primary shadow mt-2" >
@@ -74,36 +54,19 @@ exchangeCalculate.addEventListener("click", function(){
         </div>
         `
    resultValue.innerHTML = valueResult
-
-
-
-
-
    for(let otherExchange in outpot){
-    console.log(otherExchange, outpot[otherExchange])
-
     resultValue2.innerHTML += `
     <div class =  " card border-primary d-inline-flex ava mt-2"> 
     <ul style="list-style-type:none" class="list-group list-group-horizontal d-inline-flex">
       <li class="list-group-item float-end">
       ${otherExchange}:${outpot[otherExchange]}
       </li>
-
-
     </ul>
-    
     </div>
-    
     `
-
-
-
    }
 
-
    document.querySelector("#load").style.display="none"
-
-
 
     }
     setTimeout(()=>{
